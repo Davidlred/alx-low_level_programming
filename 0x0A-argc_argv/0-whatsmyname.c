43a292f3e604deba54159748dcc8accdaae89006
davidlred <davidprincewill20@gmail.com>
2023-06-09 14:21:45 -0700

    print and return

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main - a function that printf a word followed by a new line
+ * _putchar: a funtion that prints out a value passed in
+ * argv: an array of command linr input
+ * argc: the number of element in the array
+ * return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc >= 0)
+	       printf("%s \n", argv[0]);
+	
+	return (0);
+
+}
+
