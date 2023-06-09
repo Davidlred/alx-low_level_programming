2f1a81e1b73f3d0e3e820d68094ba3c621e42f6a
davidlred <davidprincewill20@gmail.com>
2023-06-09 15:01:41 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - main - a function that printf a word followed by a new line
- * _putchar: a funtion that prints out a value passed in
- * argv: an array of command linr input
- * argc: the number of element in the array
- * return: always 0
+ * main - a function that printf a word followed by a new line
+ * @argc: length of array plus 1
+ * @argv: an array of command line input
+ * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	for(i = 0; i < argc; i++)
-	       printf("%s \n", argv[0]);
-	
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s \n", argv[0]);
+
 	return (0);
 
 }
