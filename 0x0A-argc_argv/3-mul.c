d41a1a09cfcc2864d8aa74de6964377a0f28b3a5
davidlred <davidprincewill20@gmail.com>
2023-06-09 18:07:23 -0700

    multiplies two comand line numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * mul - return multiplication
+ * @a: stores an int a
+ * @b: stores an int b
+ * Return: always sucess
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * main - main function
+ * @argc: the usual
+ * @argv: the other usual
+ * Return: always sucesss
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+
+	int b = atoi(argv[2]);
+
+	int result = mul(a, b);
+
+	if  (argc <= 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", result);
+	}
+
+	return (0);
+
+}
+
+
